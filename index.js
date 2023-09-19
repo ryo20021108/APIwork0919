4b579ee0e36593debb34bdfcc7edadc0204c24d1
ryo20021108 <same20021108@gmail.com>
2023-09-19 15:44:33 +0800

    new update

---
+++ index.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'https://webdev.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/movies/'
+const POSTER_URL = BASE_URL + '/poster/'
+
+const movies = []
+
+axios.get(INDEX_URL)
+  .then((response) => {
+    movies.push(...response.data.results)
+    console.log(movies)
+  })
+  .catch((err) => console.log(err))
