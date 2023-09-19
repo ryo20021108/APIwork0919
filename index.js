39c19a2157617bcc8d5794b32735b8f8a164b6de
ryo-desktop <same20021108@gmail.com>
2023-09-19 23:13:25 +0800

    -data-panel done

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const BASE_URL = 'https://webdev.alphacamp.io'
 const INDEX_URL = BASE_URL + '/api/movies/'
-const POSTER_URL = BASE_URL + '/poster/'
-
+const POSTER_URL = BASE_URL + '/posters/'
 const movies = []
+
+const dataPanel = document.querySelector('#data-panel')
+
+function renderMovieList(data) {
+  let rawHTML = ''
+
+  data.forEach((item) => {
+    rawHTML += `
+  <div class="col-sm-3">
+        <div class="mb-2">
+          <div class="card">
+            <img
+              src="${POSTER_URL}${item.image}"
+              class="card-img-top" alt="Movie Poster" />
+            <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+            </div>
+            <div class="card-footer">
+              <button class="btn btn-primary btn-show-movie">More</button>
+              <button class="btn btn-info btn-add-favorite">+</button>
+            </div>
+          </div>
+        </div>
+      </div>`
+  });
+  dataPanel.innerHTML = rawHTML
+}
+
 
 axios.get(INDEX_URL)
   .then((response) => {
     movies.push(...response.data.results)
     console.log(movies)
+    renderMovieList(movies)
   })
   .catch((err) => console.log(err))
 
