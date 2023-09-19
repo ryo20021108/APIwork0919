3e28cb143b7f615af28ff55a172bd2caf3c6a609
ryo-desktop <same20021108@gmail.com>
2023-09-20 00:45:29 +0800

    - movieModal done

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const dataPanel = document.querySelector('#data-panel')
 
 function renderMovieList(data) {
   let rawHTML = ''
-
   data.forEach((item) => {
     rawHTML += `
   <div class="col-sm-3">
@@ -20,7 +19,7 @@ function renderMovieList(data) {
               <h5 class="card-title">${item.title}</h5>
             </div>
             <div class="card-footer">
-              <button class="btn btn-primary btn-show-movie">More</button>
+              <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${item.id}" > More </button>
               <button class="btn btn-info btn-add-favorite">+</button>
             </div>
           </div>
@@ -30,6 +29,30 @@ function renderMovieList(data) {
   dataPanel.innerHTML = rawHTML
 }
 
+function showMovieModal(id) {
+  const movieTitle = document.querySelector('#movie-modal-title')
+  const movieImage = document.querySelector('#movie-modal-image')
+  const movieDate = document.querySelector('#movie-modal-date')
+  const movieDescription = document.querySelector('#movie-modal-description')
+  axios.get(INDEX_URL + id)
+    .then(response => {
+      const data = response.data.results
+      movieTitle.innerText = data.title
+      movieDate.innerText = data.release_date
+      movieDescription.innerText = data.description
+      movieImage.innerHTML = `<img src="${POSTER_URL}${data.image}" alt="movie-poster"
+                class="img-fluid">`
+    })
+
+
+}
+
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  let target = event.target
+  if (target.matches('.btn-show-movie')) {
+    showMovieModal(Number(target.dataset.id))
+  }
+})
 
 axios.get(INDEX_URL)
   .then((response) => {
